cd1b4d354df3ae4ebb81106fbe348d6784b06486
sherazshaikh <shaikhsheraz7@gmail.com>
2020-12-23 23:11:16 +0500

    registration detaile page completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react"
 import './App.css';
 import SignUp from "./Component/SignUp";
+import Registration from "./Component/Registration"
 
 function App() {
   return (
     <div className="App">
-     <SignUp />
+     <Registration />
     </div>
   );
 }
--- src/Component/Registration.js
+++ src/Component/Registration.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./registration.css";
 
 const Registration = () => {
-    return (
-        <div className="registor">
-            
+  return (
+    <div className="registor">
+      <div className="triangle" />
+      <div className="registor_detail">
+        <div className="detail">
+          <h2>Completa tu registro</h2>
+          <div className="detail_input">
+            <input type="text" placeholder="Full name with capitle letters" />
+            <input type="text" placeholder="Administrator's Cve" />
+            <input type="text" placeholder="Korea electronics" />
+            <div className="career__cellPhone">
+              <input type="text " placeholder="career" />
+              <input type="text" placeholder="cell phone" />
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Registration
+export default Registration;
